d3f2c65344ea1bb94772264427f5e89ead6af807
north-cyprus-exists <85763817+north-cyprus-exists@users.noreply.github.com>
2021-06-17 21:36:54 +0100

    Reverted scroll into view

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ function mpcall(){
 
             document.getElementById("input1_1").value = Letter;
 
-            var elmnt = document.getElementById("mpimg");
+            var elmnt = document.getElementById("mpconstit");
             elmnt.scrollIntoView();}
 
 
